96a92c7307b1b7ce5a233ddc6d715579fdbbf1f3
Notch0815 <63363133+Notch0815@users.noreply.github.com>
2020-04-09 01:37:32 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setupools import setup, find_packages
+
+setup(
+    name='mytestlib-ora',
+    version='1.0.0', 
+    url='https://pypi.org',
+    author='M.K',
+    author_email='saikyou@gmail.com',
+    description='My Test Library',
+    packages=find_packages()
+)
